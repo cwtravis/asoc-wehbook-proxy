104c55b6f00f3fed911b98eab6f4c776953d4435
Chill <chill@chillaspect.com>
2021-07-28 22:43:51 -0500

    Initial Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,96 @@
+
+## AppScan on Cloud Webhook Proxy
+
+This tool creates an endpoint for capturing AppScan on Cloud webhook requests, gathers additional data about the scan, creates a custom HTTP request based on a provided template, then reroutes it to the desired endpoints. By default, AppScan on Cloud only provides a scan execution ID to any listening endpoints. This scan execution ID needs to have further context to be useful, for alerting or reporting purposes. As an example, I have provided a template to create an alert message on a desired Discord channel. 
+
+## Requirements
+
+This tool was written for Python3 and was tested against 3.9.0. 
+Packages in use that will need to be installed:
+1. Flask - Web server routing
+2. Python Requests - API calls to ASoC
+
+Install on Windows
+```
+py -m pip install Flask
+py -m pip install requests
+```
+Install on Linux (pip or pip3 depending on your setup)
+```
+pip3 install Flask
+pip3 install requests
+```
+
+## Setup and run ASoC Webhook Proxy
+
+The FLASK_ENV can be development or production. I use development while testing demonstrating.
+
+### Windows
+
+```
+git clone {repisitory_link}
+cd asoc_wehbook_proxy
+set FLASK_APP=asoc_webhook_proxy
+set FLASK_ENV=development
+py -m flask run --host=0.0.0.0 --port=5000
+```
+
+### Linux
+```
+git clone {repisitory_link}
+cd asoc_wehbook_proxy
+export FLASK_APP=asoc_webhook_proxy
+export FLASK_ENV=development
+flask run --host=0.0.0.0 --port=5000
+```
+Leave off the host to run on localhost 127.0.0.1 or leave off the port to run on the default port 5000. ASoC Webhook Proxy will read the config file and listen on the indicated endpoints for webhook requests from ASoC. 
+
+## Prerequisites
+
+The config.json file needs to contain the following (see example):
+1. Hostname and port of the running server
+2. ASoC API Key
+	a. This Account needs permissions to create/view webhooks and have visibility to the AssetGroups in ASoC required to pull scan data. If this is global, be sure that the account has permissions to view ALL asset groups scans.
+3. Webhook Data
+
+A template file that matches the Name of the webhook in the config file. In my example I use Discord. There is a discord template in the templates directory and a webhook in the config file with name "discord". This is so that ASoC can make a webook request with route "/asoc/discord/scan-execution-id" and ASoC Webhook Proxy knows to use the discord template and match the webhook URL from the config file with the discord webhook.
+
+ASoC Webhook Proxy will attempt to create a webhook in ASoC if one doesn't exist for one found in the config.json file.
+
+## Template Fields
+Check the discord example template to see the fields in action. 
+| Field | Meaning |
+|--|--|
+| {app} | Scanned Application Name |
+| {report_url} | URL of the downloaded report |
+| {NIssuesFound} | Number of issues found during the scan |
+| {NHighIssues} | Number of high severity issues found during the scan |
+| {NMediumIssues} | Number of medium severity issues found during the scan |
+| {NLowIssues} | Number of low severity issues found during the scan |
+| {scanName} | Name of the scan in ASoC |
+| {duration_str} | Duration of the scan in 0h 0m 0s |
+| {createdBy} | Name and email of user that created the scan |
+| {time_stamp} | Timestamp of when the webhook was received |
+
+Discord Example
+To setup your Discord server to receive webhooks, right click your server and select Server Settings > Integrations > View Webhooks > New Webhook. Give your webhook a name to post under, select an channel for it to post to, and optionally select an avatar image. 
+
+![Discord Example 1](http://chillaspect.com/images/asoc_whp2.png)
+
+Click "Copy Webhook URL" to copy the URL to your clipboard and paste into your config file in "url" under the "discord" webhook. Also update the following fields in the config file:
+1. Add ASoC API Key
+2. Update Hostname and Port
+3. Add an AppScan Presence ID (must be an active Presence Id. 
+
+Now run ASoC Webhook Proxy and verify that a webhook is either already found or created for the discord webhook. Run a scan and watch the output from the ASoC Webhook Proxy. You should see an alert from Discord.
+
+![Discord Example 2](http://chillaspect.com/images/asoc_whp1.png)
+
+## Other Info
+
+ - ASOC Webhook Proxy will output its logs to /logs/asoc_webhook_proxy.log
+ - ASoC Webhook Proxy will download and save a scan report to /reports/{scan_execution_id}.html
+ - The Flask webserver will serve files in the reports directory.
+ - This is meant as a Proof of Concept to demonstrate what can be done with webhooks.
+ - Implement this at your own risk.
+ - Feel free to create an issue here for any problems.
---
+++ asoc_webhook_proxy.py
@@ -0,0 +1,288 @@
+from flask import Flask, request, Response, send_from_directory
+from asoc import ASoC
+from datetime import datetime
+from threading import Thread
+import re
+import sys
+import os
+import time
+import json
+import requests
+import logging
+
+level = logging.INFO
+
+#Setup Logging first
+logger = logging.getLogger('asco_webhook_proxy')
+logger.setLevel(level)
+fh = logging.FileHandler('log/asco_webhook_proxy.log')
+fh.setLevel(level)
+ch = logging.StreamHandler()
+ch.setLevel(level)
+formatter = logging.Formatter('%(asctime)s - %(threadName)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+asoc = None
+scriptDir = os.getcwd()
+config = None
+safePattern = None
+
+"""
+Initialize Globals
+Validate Config
+"""
+def init():
+    global asoc, config, safePattern, level
+    
+    logger.info("Initializing Web Hook Proxy")
+    
+    #Read the Config File config.json
+    if(not os.path.isfile("config.json")):
+        logger.error("Config (config.json) file doesn't exist! Exiting")
+        sys.exit(1)
+    try:
+        with open("config.json") as f:
+            config = json.load(f)
+    except Exception as e:
+        logger.error(e)
+        logger.error("Cannot read config file! Bad Formatting? Exiting")
+        sys.exit(1)
+    
+    logger.info("Config File Loaded")
+    
+    #Check to see if reports dir exists
+    if(not os.path.isdir("reports")):
+        #make the dir if it doesnt exist
+        try:
+            os.mkdir("reports")
+            if(not os.path.isdir("reports")):
+                logger.error("Cannot make report directory! Exiting")
+                sys.exit(1)
+        except FileExistsError:
+            #File Exists Thats Ok
+            pass
+    logger.info("Reports Directory Exists")
+    
+    #Check to see if ASoC Creds Work
+    apikey = config["asoc_api_key"]
+    asoc = ASoC(config["asoc_api_key"])
+    if(asoc.login()):
+        logger.info("ASoC Credentials OK")
+    else:
+        logger.error("ASoC: Cannot login, check creds! Exiting")
+        sys.exit(1)
+    
+    logger.info("Checking ASoC for Webhooks")
+    asocWebHooks = asoc.getWebhooks()
+    if(asocWebHooks is not None):
+        n = len(asocWebHooks)
+        logger.info(f"{n} webhooks returned")
+        for wh in config["webhooks"]:
+            wh_name = wh["name"]
+            calcConfigWHUrl = "http://"+config["hostname"]+":"+str(config["port"])+"/asoc/"+wh_name
+            found = False
+            for asocWh in asocWebHooks:
+                calcAsocUrl = asocWh["Uri"].replace("/{SubjectId}", "")
+                if(calcConfigWHUrl == calcAsocUrl):
+                    found = True
+            if(found):
+                logger.info(f"Matched webhook [wh_name] in ASoC")
+            else:
+                logger.info(f"Webhook [{wh_name}] not found in ASoC.")
+                logger.info(f"Attempting to create webhook in ASoC")
+                if(asoc.createWebhook(wh["PresenceId"],calcConfigWHUrl+"/{SubjectId}"),True):
+                    logger.info(f"Successfully created ASoC Webhook [{wh_name}]")
+                else:
+                    logger.warning("Could not create ASoC Webhook... bad permissions?")
+    safePattern = re.compile('[^a-zA-Z0-9\-_]')
+    
+    logger.info("Initialization OK")
+    
+def getScanSummary(execId):
+    global asoc
+    if(not asoc.checkAuth()):
+        if(not asoc.login()):
+            logger.error("Cannot login, check network or credentials")
+            return None
+    scanExec = asoc.scanSummary(execId, True)
+    if(not scanExec):
+        logger.error(f"Error getting scan execution summary: {execId}")
+        return None
+    scanId = scanExec["ScanId"]
+    scan = asoc.scanSummary(scanId)
+    if(not scan):
+        logger.error(f"Error getting scan summary: {scanId}")
+        return None
+    data = {
+        "scan": scan,
+        "scan_execution": scanExec
+    }
+    return data
+
+def saveReport(execId, reportConfig, fullPath):
+    if(not asoc.checkAuth()):
+        if(not asoc.login()):
+            logger.error("Cannot login, check network or credentials")
+            return False
+    reportId = asoc.startScanReport(execId, reportConfig, True)
+    if(not reportId):
+        logger.error("Error starting report for scan execution {execId}")
+        return False
+    waiting = asoc.waitForReport(reportId)
+    if(not waiting):
+        logger.error("Problem occurred waiting for report")
+        return False
+    if(not asoc.downloadReport(reportId, fullPath)):
+        logger.error("Problem occurred downloading report")
+        return False
+    return True
+
+"""
+Function to translate the scan data to the format expected by
+the webhook
+"""
+def scanDataToWebhookData(template, data, reportUrl=None):
+    now = datetime.now()
+    time_stamp = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    app = data["scan"]["AppName"]
+    duration_secs = data["scan_execution"]["ExecutionDurationSec"]
+    duration_str = time.strftime('%Hh %Mm %Ss', time.gmtime(duration_secs))
+    createdBy = data["scan_execution"]["CreatedBy"]["FirstName"]+" "
+    createdBy += data["scan_execution"]["CreatedBy"]["LastName"]+" <"
+    createdBy += data["scan_execution"]["CreatedBy"]["Email"]+">"
+    scanName = data["scan"]["Name"]
+    report_url = reportUrl
+    NIssuesFound = data["scan_execution"]["NIssuesFound"]
+    NHighIssues = data["scan_execution"]["NHighIssues"]
+    NMediumIssues = data["scan_execution"]["NMediumIssues"]
+    NLowIssues = data["scan_execution"]["NLowIssues"]
+    
+    templateStr = ""
+    try:
+        with open(template, "r") as f:
+            templateStr = f.read()
+    except Exception as e:
+        logger.error(f"Error reading template file {template}")
+        logger.error(e)
+        return None
+        
+    templateStr = templateStr.replace("{app}", app)
+    templateStr = templateStr.replace("{time_stamp}", time_stamp)
+    templateStr = templateStr.replace("{duration_str}", duration_str)
+    templateStr = templateStr.replace("{createdBy}", createdBy)
+    templateStr = templateStr.replace("{scanName}", scanName)
+    if(not reportUrl):
+        templateStr = templateStr.replace("{report_url}", "")
+    else:
+        templateStr = templateStr.replace("{report_url}", report_url)
+    templateStr = templateStr.replace("{NIssuesFound}", str(NIssuesFound))
+    templateStr = templateStr.replace("{NHighIssues}", str(NHighIssues))
+    templateStr = templateStr.replace("{NMediumIssues}", str(NMediumIssues))
+    templateStr = templateStr.replace("{NLowIssues}", str(NLowIssues))
+    
+    templateJson = None
+    try:
+        templateJson = json.loads(templateStr)
+    except Exception as e:
+        logger.error("Error parsing templateStr to Python Dict")
+        logger.error(e)
+        return None
+    return templateJson
+    
+"""
+Function to make the post request to the webhook
+"""
+def postWebhook(webhookUrl, data):
+    resp = requests.post(webhookUrl, json=data)
+    return resp.status_code
+
+def processWebhook(webhook, execId, baseUrl):
+    global scriptDir, config
+    
+    #Set the webhook fields
+    webhookUrl = webhook["url"]
+    webhookName = webhook["name"]
+    reportConfig = webhook["report_config"]
+    ext = reportConfig["Configuration"]["ReportFileType"].lower()
+ 
+    #Build the real path to save the report file
+    reportPath = f"{scriptDir}/reports/{execId}.{ext}"
+    
+    #Start pulling the scan summary from ASoC
+    logger.info(f"Processing Webhook [{webhookName}] with scan execution [{execId}]")
+    scandata = getScanSummary(execId)
+    if(not scandata):
+        return
+    
+    logger.info(f"[{webhookName}] Retrieved Scan Data Successfully")
+    
+    #Download the Report
+    if(saveReport(execId, reportConfig, reportPath)):
+        reportUrl = f"{baseUrl}reports/{execId}.{ext}"
+        logger.info(f"[{webhookName}] Calculated Report URL: {reportUrl}")
+    else:
+        reportUrl = None
+    
+    #Convert the scan data to the webhook template
+    data = scanDataToWebhookData(f"templates/{webhookName}", scandata, reportUrl)
+    
+    if(data):
+        logger.info(f"[{webhookName}] Translated Scan Data to Webhook Template")
+    else:
+        return
+        
+    #Make the webhook request
+    result = postWebhook(webhookUrl, data)
+    logger.info(f"[{webhookName}] Posted Data to Webhook, Response Code: {result}")
+    
+"""
+Define Flask App (Lazy Mode)
+"""
+app = Flask(__name__)
+
+#Catch webhook requests from ASoC
+@app.route('/asoc/<webhook>/<execId>', methods=['GET'])
+def respond(webhook, execId):
+    global safePattern, config
+    
+    #Validate the request parameters
+    validated = re.sub(safePattern, '', webhook)
+    if(validated != webhook):
+        logger.error("Invalid Chars in Webhook name. Valid = [a-Z0-9\-_]")
+        return Response(status=400)
+        
+    validated = re.sub(safePattern, '', execId)
+    if(validated != execId):
+        logger.error("Invalid Chars in Scan Exec ID name. Valid = [a-Z0-9\-_]")
+        return Response(status=400)
+    
+    
+    #Ensure the webhook template exists
+    if(not os.path.isfile(f"templates/{webhook}")):
+        logger.error(f"Template {webhook} does not exist")
+        return Response(status=400)
+    
+    #Map the webhook to a URL
+    webhookObj = None
+    for wh in config["webhooks"]:
+        if(wh["name"] == webhook):
+            webhookObj = wh
+            break
+    if(not webhookObj):
+        logger.error(f"Cannot find webhook [{webhook}] in config file")
+        return Response(status=400)
+        
+    #Move the execution to a thread and respond immediately with 202 (Request Accepted)
+    Thread(target=processWebhook, args=(webhookObj, execId, request.url_root)).start()
+    return Response(status=202)
+
+#Serve reports from the report directory
+@app.route('/reports/<path:path>')
+def send_js(path):
+    return send_from_directory('reports', path)
+    
+init()
+
